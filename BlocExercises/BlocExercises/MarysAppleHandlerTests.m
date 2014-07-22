2c5f70d955b708056489490ad4a8bf19c38647fe
Michael Boyce <michaelwboyce@gmail.com>
2014-07-21 23:08:55 -0400

    Finished the if else / ternary lesson

--- BlocExercises/BlocExercises/MarysAppleHandlerTests.m
+++ BlocExercises/BlocExercises/MarysAppleHandlerTests.m
@@ -32,79 +32,34 @@
 }
 
 - (void)testThatMaryCannotPurchaseAnythingFor4Dollars {
-    NSNumber *minimumPurchaseAmount = [NSNumber numberWithInt:5];
-    NSNumber *maryMoney = [NSNumber numberWithInt:4];
-    BOOL enoughMoney = (maryMoney >= minimumPurchaseAmount);
-    if(enoughMoney){
-        // this block will not be run
-    } else {
-        NSString *purchasableItem = @"get out of my store";
-    
-        XCTAssertEqualObjects(purchasableItem, @"get out of my store", @"If Mary has $4, she should be told to get out."); }
+    NSString *purchasableItem = [self.appleHandler itemMaryCanPurchaseForDollars:4];
+    XCTAssertEqualObjects(purchasableItem, @"get out of my store", @"If Mary has $4, she should be told to get out.");
 }
 
+
 - (void)testThatMaryCanPurchaseGumFor5Dollars {
-    NSNumber *minimumPurchaseAmount = [NSNumber numberWithInt:5];
-    NSNumber *maryMoney = [NSNumber numberWithInt:5];
-    NSString *purchasableItem;
-    BOOL enoughMoney = (maryMoney >= minimumPurchaseAmount);
-    if(enoughMoney == YES){
-        purchasableItem = @"have some gum";
-    } else {
-        purchasableItem = @"get out of my store";
+    NSString *purchasableItem = [self.appleHandler itemMaryCanPurchaseForDollars:5];
     XCTAssertEqualObjects(purchasableItem, @"have some gum", @"If Mary has $5, she should receive an offer to purchase gum.");
-    }
 }
 
 - (void)testThatMaryCanPurchaseAnAppleFor6Dollars {
-    NSNumber *minimumPurchaseAmount = [NSNumber numberWithInt:6];
-    NSNumber *maryMoney = [NSNumber numberWithInt:6];
-    NSString *purchasableItem;
-    BOOL enoughMoney = (maryMoney >= minimumPurchaseAmount);
-    if(enoughMoney == YES){
-        purchasableItem = @"have an apple";
-    } else {
-        purchasableItem = @"get out of my store";
-
+    NSString *purchasableItem = [self.appleHandler itemMaryCanPurchaseForDollars:6];
     XCTAssertEqualObjects(purchasableItem, @"have an apple", @"If Mary has $6, she should receive an offer to purchase an apple.");
-    }
-    }
+}
 
 - (void)testThatMaryCanPurchaseAnAppleComputerFor1000Dollars {
-    NSNumber *minimumPurchaseAmount = [NSNumber numberWithInt:1000];
-    NSNumber *maryMoney = [NSNumber numberWithInt:1000];
-    NSString *purchasableItem;
-    BOOL enoughMoney = (maryMoney >= minimumPurchaseAmount);
-    if(enoughMoney == YES){
-        purchasableItem = @"have an Apple computer";
-    } else {
-        purchasableItem = @"get out of my store";
-
+    NSString *purchasableItem = [self.appleHandler itemMaryCanPurchaseForDollars:1000];
     XCTAssertEqualObjects(purchasableItem, @"have an Apple computer", @"If Mary has $1000+, she should receive an offer to purchase an Apple computer.");
-
-    }
 }
 
 - (void)testThatMaryCanPurchaseTheBigAppleFor1000000000Dollars {
-    NSNumber *minimumPurchaseAmount = [NSNumber numberWithLong:1000000000];
-    NSNumber *maryMoney = [NSNumber numberWithLong:1000000000];
-    NSString *purchasableItem;
-    BOOL enoughMoney = [maryMoney isEqualToValue: minimumPurchaseAmount];
-    if(enoughMoney == YES){
-        purchasableItem = @"have The Big Apple";
-    } else {
-        purchasableItem = @"get out of my store";
-
+    NSString *purchasableItem = [self.appleHandler itemMaryCanPurchaseForDollars:1000000000];
     XCTAssertEqualObjects(purchasableItem, @"have The Big Apple", @"If Mary has $1 billion, she should receive an offer to purchase New York City (known as The Big Apple).");
-    }
 }
-
-- (void)testThatPriceOfDiscountedVodka {
+- (void)testThatPriceOfDiscountedVodkaIs18Dollars {
     self.appleHandler.getsDiscount = YES;
-    NSUInteger cost = self.appleHandler.dollarCostForAppleFlavoredVodka;
-    BOOL message = [(int) cost isEqualtoValue:18] ? @"Discounted vodka should be $18." : @"Take a Hike";
-    XCTAssertEqual(cost, 18, @"Discounted vodka should be $18.");
-}
+    NSUInteger cost = [self.appleHandler dollarCostForAppleFlavoredVodka];
+    XCTAssertEqual(cost, 18, @"Discounted vodka should be $18."); }
 
 - (void)testThatPriceOfUndiscountedVodkaIs24Dollars {
     NSUInteger cost = [self.appleHandler dollarCostForAppleFlavoredVodka];
